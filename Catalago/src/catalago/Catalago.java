b1ad84343a4bc9051601ed32ce8bd7498518e950
YaelAke <Yael@Yael>
2023-04-30 00:20:56 -0500

    Creamos los packcage y clases que vamos a utilizaqr

---
+++ Catalago/src/catalago/Catalago.java
@@ -0,0 +1,16 @@
+package catalago;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Yael
+ */
+public class Catalago {
+    public static void main(String[]atgs){
+        System.out.println("Hola");
+    }    
+}
