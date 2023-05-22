ebd3758872a6c89cd8c9f38c5c4bb8bd3c6c9d77
YaelAke <Yael@Yael>
2023-05-22 15:00:31 -0500

    Actividad 2

--- ExamenProgramacion2/src/models/TablaPeriodica.java
+++ ExamenProgramacion2/src/models/TablaPeriodica.java
@@ -9,5 +9,74 @@ package models;
  * @author Yael
  */
 public class TablaPeriodica {
+
+    private int id;
+    private String masaAtomica;
+    private String numeroAtomico;
+    private String simboloQuimico;
+    private String nombre;
+    private String color;
     
+    public TablaPeriodica() {
+
+    }
+
+    public TablaPeriodica(int id, String masaAtomica, String numeroAtomico, String simboloQuimico, String nombre, String color) {
+        this.id = id;
+        this.masaAtomica = masaAtomica;
+        this.numeroAtomico = numeroAtomico;
+        this.simboloQuimico = simboloQuimico;
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMasaAtomica() {
+        return masaAtomica;
+    }
+
+    public void setMasaAtomica(String masaAtomica) {
+        this.masaAtomica = masaAtomica;
+    }
+
+    public String getNumeroAtomico() {
+        return numeroAtomico;
+    }
+
+    public void setNumeroAtomico(String numeroAtomico) {
+        this.numeroAtomico = numeroAtomico;
+    }
+
+    public String getSimboloQuimico() {
+        return simboloQuimico;
+    }
+
+    public void setSimboloQuimico(String simboloQuimico) {
+        this.simboloQuimico = simboloQuimico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
