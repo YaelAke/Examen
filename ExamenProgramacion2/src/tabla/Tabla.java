46310f8b07915ce738ab4ed0c50aea955865ce8c
YaelAke <Yael@Yael>
2023-05-22 14:40:42 -0500

    Actividad 1

---
+++ ExamenProgramacion2/src/tabla/Tabla.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tabla;
+
+/**
+ *
+ * @author Yael
+ */
+public class Tabla {
+
+    public static void author68350(){
+        System.out.println("Nombre: Yael Del Jesus Ake Chulin");
+        System.out.println("Edad: 20");
+        System.out.println("Sexo: Hombre");
+        System.out.println("Matricula: 68350");
+    }
+}
