2cf37ea4742995e371b134dc0ba04d7d9f7f1c50
YaelAke <Yael@Yael>
2023-05-22 17:54:18 -0500

    Actividad 6 y 7 (parte 2)

--- ExamenProgramacion2/src/models/TablaPeriodica.java
+++ ExamenProgramacion2/src/models/TablaPeriodica.java
@@ -4,6 +4,8 @@
  */
 package models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Yael
@@ -16,7 +18,7 @@ public class TablaPeriodica {
     private String simboloQuimico;
     private String nombre;
     private String color;
-    
+
     public TablaPeriodica() {
 
     }
@@ -30,7 +32,6 @@ public class TablaPeriodica {
         this.color = color;
     }
 
-    
     public int getId() {
         return id;
     }
@@ -78,5 +79,50 @@ public class TablaPeriodica {
     public void setColor(String color) {
         this.color = color;
     }
+
+    @Override
+
+    public String toString() {
+
+        return "Elementos{" + "id=" + id + ", Masa Atomica=" + masaAtomica + ", Numero Atomico=" + numeroAtomico + ", Simbolo atomico=" + simboloQuimico + ", Nombre=" + nombre + ", Color=" + color + '}';
+
+    }
+
+    public static ArrayList<TablaPeriodica> tabla = new ArrayList<>();
+
+    public static void lista() {
+        for (int i = 0; i < tabla.size(); i++) {
+            System.out.println(tabla.get(i).toString());
+        }
+    }
+
+    public static void llenar() {
+        tabla.add(new TablaPeriodica(1, "20.18", "10", "Ne", "Neon", "Rosado"));
+        tabla.add(new TablaPeriodica(2, "40.08", "20", "Ca", "Calcio", "Beige"));
+        tabla.add(new TablaPeriodica(3, "65.41", "30", "Zn", "Cinc", "Beige"));
+        System.out.println(tabla);
+    }
+
+    public static void añadir(int id, String masaAtomica, String numeroAtomico, String simboloQuimico, String nombre, String color) {
+        tabla.add(new TablaPeriodica(id, masaAtomica, numeroAtomico, simboloQuimico, nombre, color));
+        System.out.println(tabla);
+    }
+
+    public static void actualizar(int recNo, int id, String masaAtomica, String numeroAtomico, String simboloQuimico, String nombre, String color) {
+        tabla.get(recNo).setId(id);
+        tabla.get(recNo).setMasaAtomica(masaAtomica);
+        tabla.get(recNo).setNumeroAtomico(numeroAtomico);
+        tabla.get(recNo).setSimboloQuimico(simboloQuimico);
+        tabla.get(recNo).setNombre(nombre);
+        tabla.get(recNo).setColor(color);
+
+    }
+
+    public static void eliminar(int id) {
+        tabla.remove(id);
+    }
+
+ 
+    
 
 }
