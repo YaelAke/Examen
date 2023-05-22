f9841ab8332c708ae9456747f8f95de2d37f6029
YaelAke <Yael@Yael>
2023-05-22 17:53:43 -0500

    Actividad 6 y 7 (parte 1)

--- ExamenProgramacion2/src/tabla/Tabla.java
+++ ExamenProgramacion2/src/tabla/Tabla.java
@@ -4,16 +4,28 @@
  */
 package tabla;
 
+import views.Principal;
+
 /**
  *
  * @author Yael
  */
 public class Tabla {
 
-    public static void author68350(){
+    public static Principal principal = new Principal();
+
+    public static void author68350() {
         System.out.println("Nombre: Yael Del Jesus Ake Chulin");
         System.out.println("Edad: 20");
         System.out.println("Sexo: Hombre");
         System.out.println("Matricula: 68350");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        author68350();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+      
     }
 }
